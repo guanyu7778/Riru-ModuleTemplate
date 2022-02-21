75501574fe2cda21b90d2b48e9bf192446604d63
guanyu7778 <44391979+guanyu7778@users.noreply.github.com>
2022-02-21 15:50:54 +0800

    frida

--- module/src/main/cpp/include/config.h
+++ module/src/main/cpp/include/config.h
@@ -1,8 +1,22 @@
 #pragma once
-
+#include <errno.h>
+#include "android/log.h"
+#define LOG_TAG    "YQFIN"
+#ifdef DEBUG
+#define LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+#else
+#define LOGD(...)
+#endif
+#define LOGV(...)  __android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, __VA_ARGS__)
+#define LOGI(...)  __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOGW(...)  __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
+#define LOGE(...)  __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#define PLOGE(fmt, args...) LOGE(fmt " failed with %d: %s", ##args, errno, strerror(errno))
 namespace riru {
     extern const int moduleVersionCode;
     extern const char* const moduleVersionName;
     extern const int moduleApiVersion;
     extern const int moduleMinApiVersion;
+    constexpr const char* kZygoteNiceName = "zygote64";
+    constexpr const char* nextLoadSo = "/data/local/yqfin.so";
 }
--- module/src/main/cpp/main.cpp
+++ module/src/main/cpp/main.cpp
@@ -1,9 +1,104 @@
 #include <jni.h>
 #include <sys/types.h>
+#include <dlfcn.h>
 #include <riru.h>
 #include <malloc.h>
 #include <cstring>
 #include <config.h>
+
+static jint my_uid = 0;
+static char package_name[256] = {0};
+static char nice_process_name[256] = {0};
+
+static bool isApp(int uid) {
+    if (uid < 0) {
+        return false;
+    }
+    int appId = uid % 100000;
+    //uid > 10000的才是app
+    return appId >= 10000 && appId <= 19999;
+}
+
+static void
+my_forkAndSpecializePre(JNIEnv *env, jint *uid, jstring *niceName, jstring *appDataDir) {
+    my_uid = *uid;
+    if (!isApp(my_uid)) {
+        return;
+    }
+
+    const char *tablePath = (env->GetStringUTFChars(*niceName, 0));
+    sprintf(nice_process_name, "%s", tablePath);
+    delete tablePath;
+
+    if (!appDataDir) {
+        LOGI("Q_M forkAndSpecializePre appDataDir null");
+        return;
+    }
+
+    const char *app_data_dir = env->GetStringUTFChars(*appDataDir, NULL);
+    if (app_data_dir == nullptr) {
+        return;
+    }
+    int user = 0;
+    if (sscanf(app_data_dir, "/data/%*[^/]/%d/%s", &user, package_name) != 2) {
+        if (sscanf(app_data_dir, "/data/%*[^/]/%s", package_name) != 1) {
+            package_name[0] = '\0';
+            LOGI("Q_M can't parse %s", app_data_dir);
+        }
+    }
+    env->ReleaseStringUTFChars(*appDataDir, app_data_dir);
+
+}
+
+static void my_forkAndSpecializePost(JNIEnv *env) {
+    if (!isApp(my_uid)) {
+        return;
+    }
+    //http://www.cplusplus.com/reference/cstdio/fread/ 读取整个文件
+    char *white_list;
+    //白名单的pkgName 最好以逗号或者分好分割开来
+    const char *filepath = "/data/local/tmp/_white_list.config";
+    FILE *fp = nullptr;
+    fp = fopen(filepath, "r");
+    if (fp != nullptr) {
+
+        fseek(fp, 0, SEEK_END);
+        int fileLen = ftell(fp);
+        white_list = (char *) malloc(sizeof(char) * (fileLen + 1));
+        fseek(fp, 0, SEEK_SET);
+        size_t count = fread(white_list, 1, fileLen, fp);
+        white_list[count] = '\0';
+        fclose(fp);
+    } else {
+        white_list = "";
+    }
+
+    if (!strstr(white_list, package_name)) {
+        return;
+    }
+
+    LOGI("Q_M xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx nice_process_name=%s, pkg=%s,uid=%d, isApp= %d",
+         nice_process_name, package_name, my_uid,
+         isApp(my_uid));
+
+    //添加这种机制，就可以提前设置进程名， 从而让frida 的gadget 能够识别到
+    jclass java_Process = env->FindClass("android/os/Process");
+    if (java_Process != nullptr && isApp(my_uid)) {
+        jmethodID mtd_setArgV0 = env->GetStaticMethodID(java_Process, "setArgV0",
+                                                        "(Ljava/lang/String;)V");
+        jstring name = env->NewStringUTF(nice_process_name);
+        env->CallStaticVoidMethod(java_Process, mtd_setArgV0, name);
+
+        void *handle = dlopen(riru::nextLoadSo, RTLD_LAZY);
+        if (!handle) {
+            //        LOGE("%s",dlerror());
+            LOGE("Q_M  %s loaded in libgadget 出错 %s", nice_process_name, dlerror());
+        } else {
+            LOGI("Q_M xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx-> %s 加载 ' %s ' 成功 ", nice_process_name,
+                 riru::nextLoadSo);
+        }
+    }
+}
 
 static void forkAndSpecializePre(
         JNIEnv *env, jclass clazz, jint *uid, jint *gid, jintArray *gids, jint *runtimeFlags,
@@ -14,6 +109,7 @@ static void forkAndSpecializePre(
     // Called "before" com_android_internal_os_Zygote_nativeForkAndSpecialize in frameworks/base/core/jni/com_android_internal_os_Zygote.cpp
     // Parameters are pointers, you can change the value of them if you want
     // Some parameters are not exist is older Android versions, in this case, they are null or 0
+    my_forkAndSpecializePre(env, uid, niceName, appDataDir);
 }
 
 static void forkAndSpecializePost(JNIEnv *env, jclass clazz, jint res) {
@@ -26,7 +122,8 @@ static void forkAndSpecializePost(JNIEnv *env, jclass clazz, jint res) {
         // When unload allowed is true, the module will be unloaded (dlclose) by Riru
         // If this modules has hooks installed, DONOT set it to true, or there will be SIGSEGV
         // This value will be automatically reset to false before the "pre" function is called
-        riru_set_unload_allowed(true);
+        // riru_set_unload_allowed(true);
+		my_forkAndSpecializePost(env);
     } else {
         // In zygote process
     }
@@ -41,6 +138,7 @@ static void specializeAppProcessPre(
     // Called "before" com_android_internal_os_Zygote_nativeSpecializeAppProcess in frameworks/base/core/jni/com_android_internal_os_Zygote.cpp
     // Parameters are pointers, you can change the value of them if you want
     // Some parameters are not exist is older Android versions, in this case, they are null or 0
+	my_forkAndSpecializePre(env, uid, niceName, appDataDir);
 }
 
 static void specializeAppProcessPost(
@@ -50,7 +148,8 @@ static void specializeAppProcessPost(
     // When unload allowed is true, the module will be unloaded (dlclose) by Riru
     // If this modules has hooks installed, DONOT set it to true, or there will be SIGSEGV
     // This value will be automatically reset to false before the "pre" function is called
-    riru_set_unload_allowed(true);
+    //riru_set_unload_allowed(true);
+	my_forkAndSpecializePost(env);
 }
 
 static void forkSystemServerPre(
@@ -105,7 +204,6 @@ RiruVersionedModuleInfo *init(Riru *riru) {
     auto core_max_api_version = riru->riruApiVersion;
     riru_api_version = core_max_api_version <= riru::moduleApiVersion ? core_max_api_version : riru::moduleApiVersion;
     module.moduleApiVersion = riru_api_version;
-
     riru_magisk_module_path = strdup(riru->magiskModulePath);
     if (riru_api_version >= 25) {
         riru_allow_unload = riru->allowUnload;
